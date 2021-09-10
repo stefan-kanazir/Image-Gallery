c50b3fe486fa4fa714db749f19d2322f3263cd9c
stefan-kanazir <kanazir.stefan@gmail.com>
2021-09-10 20:39:30 +0200

    Static card

--- src/App.js
+++ src/App.js
@@ -2,8 +2,36 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-6xl">Image Gallery</h1>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-700 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong> 500
+          </li>
+          <li>
+            <strong>Downloads:</strong> 500
+          </li>
+          <li>
+            <strong>Likes:</strong> 500
+          </li>
+        </ul>
+      </div>
+
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-grey-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-grey-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-grey-700 mr-2">
+          #tag3
+        </span>
+      </div>
     </div>
   );
 }
