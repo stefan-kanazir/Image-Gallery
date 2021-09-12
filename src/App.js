d65d875da3d627d3cd9ec60cd7c5ce189106ebd3
stefan-kanazir <kanazir.stefan@gmail.com>
2021-09-12 21:55:31 +0200

    ImageSearch component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CardImage from './components/CardImage';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -14,15 +15,19 @@ function App() {
         setIsLoading(false);
       })
       .catch(err => console.log(err))
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+      <ImageSearch searchText={(text) => setTerm(text)}/>
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading</h1> : <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:grid-cols-3">
         {images.map(image => (
           <CardImage key={image.id} image={image} />
         ))}
-      </div>
+      </div>}
     </div>
   );
 }
--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -4,34 +4,34 @@ const CardImage = ({ image }) => {
     const tags = image.tags.split(",");
 
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <div className="aspect-w-3 aspect-h-4">
-          <img src={image.webformatURL} alt="" className="w-full"/>
-        </div>
-        <div className="px-6 py-4">
-          <div className="font-bold text-purple-700 text-xl mb-2">
-            Photo by {image.user}
+        <div className="max-w-full rounded overflow-hidden shadow-lg">
+          <div className="aspect-w-3 aspect-h-4 max-h-80 overflow-hidden">
+            <img src={image.webformatURL} alt="" className="w-full"/>
           </div>
-          <ul>
-            <li>
-              <strong>Views:</strong> {image.views}
-            </li>
-            <li>
-              <strong>Downloads:</strong> {image.downloads}
-            </li>
-            <li>
-              <strong>Likes:</strong> {image.likes}
-            </li>
-          </ul>
-        </div>
-  
-        <div className="px-6 py-4">
-            {tags.map(tag => (
-                <span key={image.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-grey-700 mr-2">
-                    #{tag}
-                </span>
-            ))}
-        </div>
+          <div className="px-6 py-4">
+            <div className="font-bold text-blue-700 text-xl mb-2">
+              Photo by {image.user}
+            </div>
+            <ul>
+              <li>
+                <strong>Views:</strong> {image.views}
+              </li>
+              <li>
+                <strong>Downloads:</strong> {image.downloads}
+              </li>
+              <li>
+                <strong>Likes:</strong> {image.likes}
+              </li>
+            </ul>
+          </div>
+    
+          <div className="px-6 py-4">
+              {tags.map((tag, index) => (
+                  <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-grey-700 mr-2 md:mb-2">
+                      #{tag}
+                  </span>
+              ))}
+          </div>
       </div>
     )
 }
